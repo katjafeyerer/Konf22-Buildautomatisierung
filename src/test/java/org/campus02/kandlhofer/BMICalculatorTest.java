d5e416d97773535c7591b477a8ebeedadad376cb
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2023-02-03 20:01:51 +0100

    failing test example

--- src/test/java/org/campus02/kandlhofer/BMICalculatorTest.java
+++ src/test/java/org/campus02/kandlhofer/BMICalculatorTest.java
@@ -28,6 +28,6 @@ class BMICalculatorTest {
     @Test
     void result() {
         String result = bmiCalculator.result();
-        Assertions.assertEquals("obese", result);
+        Assertions.assertEquals("normal", result);
     }
 }
